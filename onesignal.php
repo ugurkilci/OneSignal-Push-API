db2e98ca5a0964ec5a17b1e68e405dddda3447b1
Uğur KILCI <ugurbocugu8@gmail.com>
2020-06-12 12:25:16 +0300

    URL
    Gelen web pusha tıklayınca anasayfaya yönlendiriyordu. Şimdi istediğimiz urlye yönlendiriyor.

--- onesignal.php
+++ onesignal.php
@@ -14,7 +14,9 @@
 <?php
 // UK.OS.P.API v1.0
 
-function sendMessage($apiid, $restapikey, $mesaj){
+// 
+
+function sendMessage($apiid, $restapikey, $mesaj, $url){
     $content = array(
         "en" => $mesaj
         );
@@ -24,33 +26,35 @@ function sendMessage($apiid, $restapikey, $mesaj){
         'included_segments' => array('All'),
         'data' => array("foo" => "bar"),
         'large_icon' =>"",
-        'contents' => $content
+        'contents' => $content,
+        'url' => $url
     );
 
     $fields = json_encode($fields);
-print("\nJSON sent:\n");
-print($fields);
+  
+    print("\nJSON sent:\n");
+    print($fields);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-                                               'Authorization: Basic '.$restapikey));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                                 'Authorization: Basic '.$restapikey));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_HEADER, FALSE);
+      curl_setopt($ch, CURLOPT_POST, TRUE);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);    
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+      $response = curl_exec($ch);
+      curl_close($ch);
 
-    return $response;
-}
+      return $response;
+  }
 
-$response = sendMessage("//API id", "//Rest API Key");
-$return["allresponses"] = $response;
-$return = json_encode( $return);
-print("\n\nJSON received:\n");
-print($return);
-print("\n");
+  $response = sendMessage("//API id", "//Rest API Key", "//Url");
+  $return["allresponses"] = $response;
+  $return = json_encode( $return);
+  print("\n\nJSON received:\n");
+  print($return);
+  print("\n");
 ?>
